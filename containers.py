66a52a7b1f96789dfe3e3f078452972d1b31728b
Sergey Pavlov <s.a.pavlov@gmail.com>
2024-04-11 19:29:48 +0300

    feature/multigraphs: upgrade Region

--- containers.py
+++ containers.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import numpy as np
 import pandas as pd
 import h5netcdf
@@ -6,13 +8,96 @@ from dataclasses import dataclass
 from datetime import datetime
 
 
-@dataclass
 class Region:
     """Класс для хранения координат региона"""
-    down: float
-    up: float
-    right: float
-    left: float
+
+    def __init__(self, down: float, up: float, left: float, right: float) -> None:
+        """Инициализация"""
+        self._lat = np.asarray([down, up], dtype=np.float32)
+        self._lon = np.asarray([left, right], dtype=np.float32)
+
+    def __repr__(self) -> str:
+        """Представление"""
+        coords = f"down={self.down}, up={self.up}, left={self.left}, right={self.right}"
+        return f"{self.__class__.__name__}({coords})"
+
+    @property
+    def down(self) -> float:
+        return float(self._lat[0])
+    
+    @property
+    def up(self) -> float:
+        return float(self._lat[1])
+
+    @property
+    def left(self) -> float:
+        return float(self._lon[0])
+
+    @property
+    def right(self) -> float:
+        return float(self._lon[1])
+    
+    @property
+    def lat(self) -> np.ndarray:
+        return self._lat
+
+    @property
+    def lon(self) -> np.ndarray:
+        return self._lon
+    
+    @property
+    def height(self) -> float:
+        return float(np.abs(self.up - self.down))
+
+    @property
+    def width(self) -> float:
+        return float(np.abs(self.left - self.right))
+    
+    @property
+    def coords(self) -> np.ndarray:
+        """
+        Возвращает координаты в одном двумерном массиве в формате
+        array[[down, up], [left, right]]
+        """
+        return np.vstack((self._lat, self._lon))
+    
+    @property
+    def center(self) -> np.ndarray:
+        return np.mean(self.coords, axis=1)
+
+    @staticmethod
+    def fromNumpy(lat: np.ndarray, lon: np.ndarray) -> Region:
+        """Десериализация  из массивов numpy"""
+        down, up = map(float, lat)
+        left, right = map(float, lon)
+        return Region(down, up, left, right)
+    
+    def toNumpy(self) -> tuple[np.ndarray]:
+        return self._lat, self._lon
+    
+    def addCoords(self, lat_value: float, lon_value: float) -> Region:
+        lat = self._lat + lat_value
+        lon = self._lon + lon_value
+        return Region.fromNumpy(lat, lon)
+    
+    @staticmethod
+    def regionAroundCenter(center: np.ndarray, height: float, width: float) -> Region:
+        """
+        Создает регион высотой  'height',  шириной  'width',  центр  которого находится в
+        точке 'center', и возвращает результат
+        """
+        half_height, half_width = height / 2, width / 2 
+        central_lat, central_lon = center
+        lat = np.array([central_lat - half_height, central_lat + half_height])
+        lon = np.array([central_lon - half_width, central_lon + half_width])
+        return Region.fromNumpy(lat, lon)
+
+    def centralizeRegion(self, height: float, width: float) -> Region:
+        """
+        Создает регион высотой  'height',  шириной  'width',  центр  которого совпадает с
+        центром данного региона, и возвращает результат
+        """
+        return self.regionAroundCenter(self.center, height, width)
 
 
 @dataclass
